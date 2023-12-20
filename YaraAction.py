47646c5a0103da4ea7fce7fd10dd1b07e6ab00b5
shutdown_awa <2674645691@qq.com>
2023-12-20 12:04:53 +0000

    1.添加控制层 2.优化YaraAction 3.其它功能更新

--- YaraAction.py
+++ YaraAction.py
@@ -1,6 +1,7 @@
 import yara
 import os
 import platform
+import pymysql as sql
 
 print ("\033[44m== Yara Engine API Project ==========\033[0m")
 print ("\033[44mModule: YaraAction_Module\033[0m")
@@ -24,21 +25,22 @@ def YaraRuleLoad(rulePath):
     return(rules)
 
 ## 扫描
-def YaraMatch (scanPath, rule):
+def YaraMatch (id,scanPath, rule, dbCur):
     try:
         # 扫描可执行文件
         matches = rule.match(scanPath)
         print(" \033[47m[I]\033[0m " + f"已扫描 {scanPath}")
     except yara.Error as e:
         print(" \033[41m[E]\033[0m " + f"在使用 {scanPath} 扫描 {scanPath} 时出现错误: {e}")
-    
+    #生成报告
     report = []
-    if matches:
+    if len(matches)>0:
         print(" \033[43m[W]\033[0m " + f"文件 {scanPath} 已被命中")
-    for match in matches:
-        report.add = match.rule
-    return (report)
-
-loadrule = YaraRuleLoad ("/workspaces/python/YaraEngineAPI/RuleCompiled")
-yarareport = YaraMatch ("/workspaces/python/YaraEngineAPI/ScanFile/注册码获取程序.exe", loadrule)
-print(yarareport)
+        for i in range(len(matches)):
+            report=report+matches[i]+"/"
+        #写入db
+        dbCur.execute(f"UPDATE `task` SET `matchs` = '{report}' WHERE `id` = `{str(id)}`;")
+    dbCur.execute(f"UPDATE `task` SET `status` = 'Done' WHERE `task`.`id` = 13 ;")
+    
+dbCon = sql.connect(host="192.168.0.11",user="yara",password="7QhMQ7mBB7dGs2AY",database="yara")
+YaraMatch(13,"/workspaces/python/YaraEngineAPI/ScanFile/60cfe344b06cc13231407f172c464701e3aa952588ec7d9f1396ac4ed0b631e3",YaraRuleLoad("/workspaces/python/YaraEngineAPI/RuleCompiled"),dbCon.cursor())

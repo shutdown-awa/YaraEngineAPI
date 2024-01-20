f3c8bae62c57a04dd4eb5c9c6642440a8322c4c0
shutdown_awa <2674645691@qq.com>
2024-01-20 20:40:40 +0000

    配置文件支持以及架构调整

--- backend/Controller.py
+++ backend/Controller.py
@@ -5,85 +5,36 @@ import threading
 import platform
 import yara
 import os
+import configparser
 
 print ("\033[44m== Yara Engine API Project ==========\033[0m")
-print ("\033[44mModule: Controller_Module\033[0m")
 print ("\033[44mSystem: " + platform.platform() + "\033[0m")
 print ("\033[44mPyVersion: " + platform.python_version() + "\033[0m")
 print ("\033[44mCopyright © 2023 Shutdown & Kolomina, All rights reserved.\033[0m")
 print ()
 
-# Setting
-dbHost = "192.168.0.11"
-dbUsr = "yara"
-dbPwd = "7QhMQ7mBB7dGs2AY"
-dbName = "yara"
-maxThread = 6
-scanFilePath = "/workspaces/python/YaraEngineAPI/backend/ScanFile/"
-ruleOriginPath = "/workspaces/python/YaraEngineAPI/backend/RuleOrigin"
-ruleCompiledPath = "/workspaces/python/YaraEngineAPI/backend/RuleCompiled/"
-
-## PREP
-sqlLock = threading.Lock()#创建锁
-# 登录数据库
-try:
-    with sqlLock:
-        dbCon = sql.connect(host=dbHost,user=dbUsr,password=dbPwd,database=dbName)
-    print(" \033[42m[S]\033[0m " + f"已登录到{dbUsr}@{dbHost}")
-except sql.Error as e:
-    print (" \033[45m[F]\033[0m " + f"无法登录到{dbUsr}@{dbHost}: {e}")
-    exit()
 
 
-
-## INIT
-def __init__():
-    #文件路径check
-    if (os.path.exists(scanFilePath) == False):#待扫描文件的路径
-        print (" \033[45m[F]\033[0m " + f"不存在的scanFile目录: {scanFilePath}")
-        exit()
-    if (os.path.exists(ruleOriginPath) == False):#待扫描文件的路径
-        print (" \033[45m[F]\033[0m " + f"不存在的ruleOriginPath目录: {ruleOriginPath}")
-        exit()
-    if (os.path.exists(ruleCompiledPath) == False):#待扫描文件的路径
-        print (" \033[45m[F]\033[0m " + f"不存在的ruleCompiledPath目录: {ruleCompiledPath}")
-        exit()
+# 读取配置
+def configReader():
+    global scannerMaxThread,dbHost,dbUsr,dbPwd,dbName
 
     try:
-        #重置所有Scanning状态的任务
-        with sqlLock:
-            dbCur = dbCon.cursor()
-            dbCur.execute(f"UPDATE task SET status = 'InList' WHERE status = 'Scanning';")
-            dbCon.commit()
-    except:
-        print(" \033[43m[W]\033[0m " + f"无法重设所有Scanning状态的任务，未重置的任务可能会被遗弃！")
+        config = configparser.read("./setting.ini")
+    except configparser.Error as e:
+        print(" \033[41m[E]\033[0m " + f"在读取setting.ini时出现错误: {e}")
+        exit()
+    
+    scannerRule = config.items("scanner")
+    scannerRule = dict(scannerRule)
+    scannerMaxThread = scannerRule["thread"] #最大线程
 
-    #编译规则
-    YaraRuleCompile(ruleOriginPath,ruleCompiledPath)
-
-    #进入事件
-    EventClock()
-
-
-
-## 规则编译器
-def YaraRuleCompile(src_dir, dest_dir):
-    print(" \033[47m[I]\033[0m " + "现在开始预编译规则😋")
-    # 遍历源目录
-    for filename in os.listdir(src_dir):
-        # 检查文件是否以.yar结尾
-        if filename.endswith('.yar'):
-            # 获取文件的完整路径
-            file_path = os.path.join(src_dir, filename)
-            try:
-                # 使用yara编译规则文件
-                rules = yara.compile(filepath=file_path)
-                # 将编译后的规则保存到目标目录
-                rules.save(os.path.join(dest_dir, filename))
-            except yara.Error as e:
-                print(" \033[41m[E]\033[0m " + f"在编译规则 {file_path} 时出现错误: {e}")
-                continue
-    print(" \033[42m[S]\033[0m " + "预编译已完成😋")
+    configSql = config.items("sql")
+    configSql = dict(configSql)
+    dbHost = configSql["host"] #数据库服务器
+    dbUsr = configSql["user"] #数据库用户
+    dbPwd = configSql["password"] #数据库密码
+    dbName = configSql["name"] #数据库名字
 
 
 
@@ -112,7 +63,7 @@ def ThreadStarter(startTotal, taskList):
 
 ##计划任务
 def EventClock():
-    print(" \033[42m[S]\033[0m " + "✅计划任务已处于活跃状态")
+    print(" \033[42m[S]\033[0m " + "✅Controller计划任务已处于活跃状态")
     with sqlLock:
         dbCur = dbCon.cursor()
     # 有没有新任务
@@ -120,14 +71,14 @@ def EventClock():
         time.sleep (5)
         with sqlLock:
             dbCon.commit()
-            dbCur.execute("SELECT * FROM `task` WHERE status = 'InList';")
+            dbCur.execute("SELECT * FROM `file` WHERE status = 'InList';")
         inListTask = list(dbCur.fetchall())
         # 还有没有空闲线程
         if len(inListTask) > 0:
             with sqlLock:
-                dbCur.execute("SELECT * FROM `task` WHERE status = 'Scanning';")
+                dbCur.execute("SELECT * FROM `file` WHERE status = 'Scanning';")
             scanningTask = list(dbCur.fetchall())
-            freeThread = maxThread - len(scanningTask)
+            freeThread = scannerMaxThread - len(scanningTask)
             #判断开几个线程
             if freeThread<len(inListTask):
                 startTotal=freeThread
@@ -137,4 +88,27 @@ def EventClock():
 
 
 
-__init__()
+# 载入配置
+configReader()
+
+# 登录数据库
+sqlLock = threading.Lock()#创建锁
+try:
+    with sqlLock:
+        dbCon = sql.connect(host=dbHost,user=dbUsr,password=dbPwd,database=dbName)
+    print(" \033[42m[S]\033[0m " + f"已登录到{dbUsr}@{dbHost}")
+except sql.Error as e:
+    print (" \033[45m[F]\033[0m " + f"无法登录到{dbUsr}@{dbHost}: {e}")
+    exit()
+
+# 重置所有Scanning状态的任务
+try:
+    with sqlLock:
+        dbCur = dbCon.cursor()
+        dbCur.execute(f"UPDATE file SET status = 'InList' WHERE status = 'Scanning';")
+        dbCon.commit()
+except:
+    print(" \033[43m[W]\033[0m " + f"无法重设所有Scanning状态的任务，未重置的任务可能会被遗弃！")
+
+# 进入计划任务
+EventClock()
--- backend/YaraAction.py
+++ backend/YaraAction.py
@@ -3,6 +3,8 @@ import os
 import platform
 import pymysql as sql
 import time
+import threading
+import configparser
 
 print ("\033[44m== Yara Engine API Project ==========\033[0m")
 print ("\033[44mModule: YaraAction_Module\033[0m")
@@ -13,6 +15,63 @@ print ("\033[44mCopyright © 2023 Shutdown & Kolomina, All rights reserved.\033[
 print ()
 
 
+# 读取配置
+config = configparser.read("./setting.ini")
+
+configRule = config.items("rule")
+configRule = dict(configRule)
+yaraRuleOriginDir = configRule["src_dir"] #yara规则未编译目录
+yaraRuleCompileDir = configRule["dest_dir"] #yara规则编译目录
+
+configSql = config.items("sql")
+configSql = dict(configSql)
+dbHost = configSql["host"] #数据库服务器
+dbUsr = configSql["user"] #数据库用户
+dbPwd = configSql["password"] #数据库密码
+dbName = configSql["name"] #数据库名字
+
+
+
+## PREP
+sqlLock = threading.Lock()#创建锁
+# 登录数据库
+try:
+    with sqlLock:
+        dbCon = sql.connect(host=dbHost,user=dbUsr,password=dbPwd,database=dbName)
+    print(" \033[42m[S]\033[0m " + f"已登录到{dbUsr}@{dbHost}")
+except sql.Error as e:
+    print (" \033[45m[F]\033[0m " + f"无法登录到{dbUsr}@{dbHost}: {e}")
+    exit()
+
+
+## 规则编译器
+def YaraRuleCompile(src_dir, dest_dir):
+    print(" \033[47m[I]\033[0m " + "现在开始预编译规则😋")
+
+    # 获取版本信息
+    versionInfoAtSrc = os.read(os.open(src_dir+"/version"))
+    versionInfoAtDst = os.read(os.open(dest_dir+"/version"))
+    if versionInfoAtSrc == versionInfoAtDst:
+        return
+    
+    print("发现规则更新🥳！重新编译～～～～～～")
+    # 遍历源目录
+    for filename in os.listdir(src_dir):
+        # 检查文件是否以.yar结尾
+        if filename.endswith('.yar'):
+            # 获取文件的完整路径
+            file_path = os.path.join(src_dir, filename)
+            try:
+                # 使用yara编译规则文件
+                rules = yara.compile(filepath=file_path)
+                # 将编译后的规则保存到目标目录
+                rules.save(os.path.join(dest_dir, filename))
+                print(" \033[42m[S]\033[0m " + "预编译已完成😋")
+            except yara.Error as e:
+                print(" \033[41m[E]\033[0m " + f"在编译规则 {file_path} 时出现错误: {e}")
+                continue
+
+
 ## 加载规则
 def YaraRuleLoad(rulePath):
     for root, dirs, files in os.walk(rulePath):
@@ -21,19 +80,17 @@ def YaraRuleLoad(rulePath):
                 filepath = os.path.join(root, file)
                 try:
                     rules = yara.load(filepath)
-                    print(" \033[42m[S]\033[0m " + f"已载入规则 {filepath}")
                 except yara.Error as e:
                     print(" \033[41m[E]\033[0m " + f"在载入规则 {filepath} 时出现错误: {e}")
     return(rules)
 
 ## 扫描
-def YaraMatch (id,filePath, rule, dbCon, sqlLock):
+def YaraMatch (id,filePath):
     with sqlLock:
         dbCur=dbCon.cursor()
     try:
         # 扫描文件
         matches = rule.match(filePath)
-        print(" \033[47m[I]\033[0m " + f"已扫描 {filePath}")
     except yara.Error as e:
         print(" \033[41m[E]\033[0m " + f"在使用 {filePath} 扫描 {filePath} 时出现错误: {e}")
         with sqlLock:
@@ -55,3 +112,9 @@ def YaraMatch (id,filePath, rule, dbCon, sqlLock):
         dbCur.execute (f"UPDATE `task` SET `status` = 'Done' WHERE `id` = {id};")
         dbCur.execute(f"UPDATE task SET endTime = '{int(time.time())}' WHERE id = {id};")
         dbCon.commit()
+
+
+## 事件服务
+        def EventClock():
+            print(" \033[42m[S]\033[0m " + "✅Controller计划任务已处于活跃状态!")
+            
